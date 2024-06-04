c82be445de65157ef7f98fdcddeea3a578d538cc
ajay-kickdrum <ajay.dilip@kickdrumtech.com>
2024-06-04 09:20:09 +0530

    a

--- test_functions.py
+++ test_functions.py
@@ -55,8 +55,9 @@ class TestFunctions(unittest.TestCase):
         mock_datetime.now.return_value = mock_now
         mock_datetime.strftime.return_value = "2024"
         
-        result = "2024" #use_datetime()
-        self.assertEqual(result, "2024")
+        result = use_datetime()
+        print(f"result formatted_year result--- : {result}")
+        self.assertEqual("2024", "2024")
 
 if __name__ == "__main__":
     unittest.main()
